36be0ce698bf7942ac9cf6568e2d6c2e7abd049e
Manasa Rebba <rebbamanasa26@gmail.com>
2026-01-17 16:12:23 +0530

    first commit

--- app.py
+++ app.py
@@ -334,7 +334,6 @@ st.markdown("""
 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 """, unsafe_allow_html=True)
 
-
 # PAGE NAVIGATION (SIDEBAR)
 
 st.sidebar.title("Navigation")
